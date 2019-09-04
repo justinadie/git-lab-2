7325aef4e1ad75ee6deda5651134d3bc0e0de452
Justin Adie <ja585618@ohio.edu>
2019-09-04 17:01:32 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Justin Adie
+ *        Date: 9/4/2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
